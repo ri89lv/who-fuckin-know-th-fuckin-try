0959e1c7482894936810c7396d4a8dcc01363515
RuLe0G <khlebnikov2015@yandex.ru>
2019-03-26 12:28:11 +0700

    fix1

--- lab3 pleeeeeasse/Program.cs
+++ lab3 pleeeeeasse/Program.cs
@@ -13,6 +13,7 @@ namespace lab3_pleeeeeasse
 
             int A = int.Parse(Console.ReadLine());
             int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("A-B={0}", A - B);
             //TUT BUDET GOROD SAD
             Console.ReadKey();
         }
